6801b24f6dbd59458baf502753c14cc778533bc3
Seb GG <sebastien.giguet@ynov.com>
2025-06-09 13:12:06 +0200

    add_disk

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -38,7 +38,7 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443"]
+    ports    = ["22", "80", "443", "8080"]
   }
 
   source_ranges = ["0.0.0.0/0"]
